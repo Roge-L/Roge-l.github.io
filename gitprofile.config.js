413e2e697052dc1d8b1f9b9ce2c04bfcd3afe41a
Roger Lam <36053941+Roge-L@users.noreply.github.com>
2023-05-25 23:46:22 -0400

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -109,20 +109,20 @@ const config = {
       link: 'https://roge-l.github.io/throwback-shack/',
     },
   ],
-//   // Display blog posts from your medium or dev account. (Optional)
-//   blog: {
-//     source: 'dev', // medium | dev
-//     username: 'arifszn', // to hide blog section, keep it empty
-//     limit: 2, // How many posts to display. Max is 10.
-//   },
-//   googleAnalytics: {
-//     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-//   },
-//   // Track visitor interaction and behavior. https://www.hotjar.com
-//   hotjar: {
-//     id: '',
-//     snippetVersion: 6,
-//   },
+  // Display blog posts from your medium or dev account. (Optional)
+  blog: {
+    source: 'dev', // medium | dev
+    username: '', // to hide blog section, keep it empty
+    limit: 2, // How many posts to display. Max is 10.
+  },
+  googleAnalytics: {
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  },
+  // Track visitor interaction and behavior. https://www.hotjar.com
+  hotjar: {
+    id: '',
+    snippetVersion: 6,
+  },
   themeConfig: {
     defaultTheme: 'winter',
 
